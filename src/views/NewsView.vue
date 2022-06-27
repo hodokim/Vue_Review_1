dac57f35f08cc58de42a95264245f2c770eab280
hodokim <hodo323@naver.com>
2022-06-27 22:51:47 +0900

    Header Bar 만들기, axios 설치

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    news
+    <div v-for="news in news" :key="news">{{ news.title }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data() {
+    return {
+      news: []
+    }
+  },
+
+  created() {
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    .then( res => {
+      this.news = res.data;
+    })
+    .catch()
+  }
 
 }
 </script>
