f650bb6baa9aee6b217b1dcc8591ce51ba0e5ec0
hodokim <hodo323@naver.com>
2022-07-29 23:16:45 +0900

    Ask, Jobs 스타일링

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <p v-for="item in fetchedJobs" :key="item.id">
-      <a :href="item.url">{{ item.title }}</a>
-      <small>{{item.time_ago}}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedJobs" :key="item.id" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title"> 
+          <a :href="item.url" >
+            {{ item.title }}
+          </a>
+          </p>
+          <small>
+            {{item.time_ago}} by 
+            <a :href="item.url">{{item.domain}} </a>
+          </small>
+        </div>
+      </li>
+    </ul>    
   </div>
 </template>
 
@@ -28,6 +42,27 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.news-title {
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
 
 </style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -11,33 +11,13 @@
             {{ item.title }}
           </a>
           </p>
-          <small>
+          <small class="link-text">
             by 
             <router-link :to="`/user/${item.user}`"> {{item.user}} </router-link>
           </small>
         </div>
-        
-        
-        
-        <!-- <a :href="item.url">
-          {{ item.title }}
-        </a>
-        <small>{{ item.time_ago }} by
-          <router-link :to="`/user/${item.user}`">
-            {{ item.user }}
-          </router-link>
-        </small>-->
       </li>
     </ul> 
-
-    <!-- <p v-for="item in this.$store.state.news" :key="item.id">
-      <a :href="item.url">
-        {{ item.title }}
-      </a>
-      <small>{{item.time_ago}} by 
-      <router-link :to="`/user/${item.user}`"> {{item.user}} </router-link>
-      </small>
-    </p> -->
   </div>
 </template>
 
@@ -72,6 +52,6 @@ export default {
   color: #42b883;
 }
 .link-text {
-  
+  color: #828282;
 }
 </style>
