4936c4076cda6c40acf1c720abc96e8fe3d9a594
hodokim <hodo323@naver.com>
2022-08-01 22:05:25 +0900

    공통컴포넌트 구현 [ template 속성 이용하기, v-if 이용하기]

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,23 +1,38 @@
 <template>
   <div>
     <ul class="news-list">
-      <li v-for="item in this.$store.state.news" :key="item.id" class="post">
+      <li v-for="item in listItems" :key="item.id" class="post">
         <div class="points">
-          {{ item.points }}
+          {{ item.points || 0 }}
         </div>
         <div>
-          <p class="news-title"> 
-          <a :href="item.url">
-            {{ item.title }}
-          </a>
+          <!-- 타이틀 영역 -->
+          <p class="news-title">
+            <template v-if="item.domain">
+              <a :href="item.url">
+                {{ item.title }}
+              </a>
+            </template>
+            
+            <template v-else>
+              <router-link :to="`item/${item.id}`">
+                {{ item.title }}
+              </router-link>
+            </template>
           </p>
+
           <small>
-            by 
-            <router-link :to="`/user/${item.user}`"> {{item.user}} </router-link>
+            {{ item.time_ago}} by
+            <router-link v-if="item.user" :to="`/user/${item.user}`">
+              {{ item.user }}
+            </router-link>
+            <a v-else :href="item.url">
+              {{ item.domain }}
+            </a>
           </small>
         </div>
       </li>
-    </ul> 
+    </ul>
   </div>
 </template>
 
@@ -25,17 +40,27 @@
 export default {
   created() {
     const name = this.$route.name;
-    if (name === 'news'){
-        this.$route.dispatch("FETCH_NEWS");
-    } else if (name === 'ask') {
-        this.$route.dispatch("FETCH_ASK");
-    } else if (name === 'jobs') {
-        this.$route.dispatch("FETCH_JOBS");
+    if (name === "news") {
+      this.$store.dispatch("FETCH_NEWS");
+    } else if (name === "ask") {
+      this.$store.dispatch("FETCH_ASK");
+    } else if (name === "jobs") {
+      this.$store.dispatch("FETCH_JOBS");
     }
   },
-  computed : {
-    
-  }
+  computed: {
+    listItems() {
+      const name = this.$route.name;
+      if (name === "news") {
+        return this.$store.state.news;
+      } else if (name === "ask") {
+        return this.$store.state.ask;
+      } else if (name === "jobs") {
+        return this.$store.state.jobs;
+      }
+      return 'news';
+    },
+  },
 };
 </script>
 
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,68 +1,17 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li v-for="item in fetchedJobs" :key="item.id" class="post">
-        <div class="points">
-          {{ item.points || 0 }}
-        </div>
-        <div>
-          <p class="news-title"> 
-          <a :href="item.url" >
-            {{ item.title }}
-          </a>
-          </p>
-          <small>
-            {{item.time_ago}} by 
-            <a :href="item.url">{{item.domain}} </a>
-          </small>
-        </div>
-      </li>
-    </ul>    
+    <list-item></list-item> 
   </div>
 </template>
 
 <script>
-import { mapGetters} from 'vuex';
-
-export default {
-  computed: {
-    ...mapGetters([
-      'fetchedJobs'
-    ])
-
-    // mapState를 활용한 방법
-    // ...mapState({
-    //   jobs: state => state.jobs
-    // })
-  },
-  created() {
-    this.$store.dispatch('FETCH_JOBS');
+import ListItem from "@/components/ListItem.vue";
+export default  {
+  components :{
+    ListItem,
   }
-
 }
 </script>
 
 <style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-.news-title {
-  margin: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-
 </style>
